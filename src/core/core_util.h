38f9c555876a8c849a303b34c2417c26808d7af5
borcun <borcunozkablan@gmail.com>
2024-03-23 00:33:40 +0300

    nanogui & sims moved other base

--- src/core/core_util.h
+++ src/core/core_util.h
@@ -10,7 +10,8 @@
 
 /// minimum period of task in usec
 #define TASK_MIN_PERIOD (100000U)
-
+/// maximum message length
+#define MAX_MSG_LEN (256U)
 /// pubsub node location
 #define NODE_DIR ((const char *) "ipc:///tmp/")
 
