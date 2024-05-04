c32b66e25feb267a7f9df517359ec84ab9510d28
borcun <borcunozkablan@gmail.com>
2024-05-04 14:05:18 +0300

    removed logger sample, added cmake files of samples into master cmake

---
+++ sample/network/src/network.cmake
@@ -0,0 +1,2 @@
+cmake_minimum_required(VERSION 3.2)
+set(NETWORK_SRC src/serial_device.cpp src/gps.cpp src/dcu.cpp src/main.cpp)
---
+++ sample/pipeline/src/pipeline.cmake
@@ -0,0 +1,2 @@
+cmake_minimum_required(VERSION 3.2)
+set(PIPELINE_SRC src/serial_device.cpp src/gps.cpp src/dcu.cpp src/main.cpp)
